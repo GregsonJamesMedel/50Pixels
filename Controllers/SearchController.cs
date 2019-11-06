cc09947d4d0cfde71b5626cd025e4e6dedc78969
GregsonJamesMedel <GregsonJamesMedel@Gmail.com>
2019-11-06 19:49:11 +0800

    add search people feature

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,5 +1,8 @@
+using System.Linq;
+using _50Pixels.Models;
 using _50Pixels.Services;
 using _50Pixels.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _50Pixels.Controllers
@@ -7,15 +10,22 @@ namespace _50Pixels.Controllers
     public class SearchController : Controller
     {
         private readonly IPhotoService _photoService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public SearchController(IPhotoService photoService)
+        public SearchController(UserManager<ApplicationUser> userManager, IPhotoService photoService)
         {
+            this._userManager = userManager;
             this._photoService = photoService;
         }
 
         public IActionResult Results(SearchResultsViewModel searchVM)
         {
             searchVM.Photos = this._photoService.SearchPhotoByTitle(searchVM.SearchKey);
+
+            if (searchVM.SearchType == 1.ToString())
+                    searchVM.ApplicationUsers = this._userManager.Users
+                    .Where(x => x.Firstname.Contains(searchVM.SearchKey) || x.Lastname.Contains(searchVM.SearchKey));
+
             return View(searchVM);
         }
     }
--- ViewModels/SearchResultsViewModel.cs
+++ ViewModels/SearchResultsViewModel.cs
@@ -8,5 +8,6 @@ namespace _50Pixels.ViewModels
         public string SearchKey { get; set; }
         public string SearchType { get; set; }
         public IEnumerable<Photo> Photos { get; set; }
+        public IEnumerable<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
